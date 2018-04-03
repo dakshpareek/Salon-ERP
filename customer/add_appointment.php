926d10e39c2b99e805c98af03dd7897c0cf5daac
dakshpareek <unknowndaksh@gmail.com>
2018-04-03 18:25:12 +0530

    New Update

--- customer/add_appointment.php
+++ customer/add_appointment.php
@@ -28,6 +28,7 @@ $ename=mysqli_real_escape_string($conn,$_POST['name']);
 $email=mysqli_real_escape_string($conn,$_POST['mail']);
 $phone=mysqli_real_escape_string($conn,$_POST['phone']);
 $address=mysqli_real_escape_string($conn,$_POST['address']);
+print($phone);
 $tm=$_POST['time'];
 $gen=$_POST['gen'];
 
@@ -37,15 +38,16 @@ $the=implode(',', $employee);
 $ser=implode(',', $services);
 $now=date('Y-m-d');
 
+/*
 if($phone='')
 {
 $phone=0;
 }
-
+*/
 $sql2 = "insert into active(ename,phone,therapy,therapist,dt,start,gender) values ('$ename','$phone','$ser','$the','$now','$tm','$gen')";
 #echo $sql2;
 $result2 = mysqli_query($conn, $sql2);
-echo "<script>alert('Appointment Added..!');location.href='index.php';</script>";
+#echo "<script>alert('Appointment Added..!');location.href='index.php';</script>";
 echo mysqli_error($conn);
  //echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
 ?>
